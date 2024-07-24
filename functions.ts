ed0e4536e2d2924aa91dc2cef7bf205edc4285df
Kingmancha <mgabrielvr123@gmail.com>
2024-07-24 15:37:31 -0300

    commit inicial

---
+++ functions.ts
@@ -0,0 +1,18 @@
+
+
+
+function multiplicacao(a: number, b: number): number {
+    return a * b;
+}
+
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+
+console.log(multiplicacao(2, 3)); 
+console.log(saudacao('João'));    
+
+
+export { multiplicacao, saudacao };
